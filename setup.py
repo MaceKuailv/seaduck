d35cb86bdeb2447ba2c58480cb0063fd1fed1c41
MaceKuailv <52629492+MaceKuailv@users.noreply.github.com>
2023-01-23 20:31:18 -0500

    Now you can pip install -e it

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'OceInterp',
+    version = '0.1.0',
+    description = 'A python package that does interpolation and lagrangian simulation',
+    author='Wenrui Jiang',
+    author_email='wjiang33@jhu.edu',
+    url = 'https://github.com/MaceKuailv/OceInterp',
+    packages = find_packages()
+)
