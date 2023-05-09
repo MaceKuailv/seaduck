6a4df724e79192f5965667e020a75f51e689d570
Wenrui Jiang <wjiang33@jhu.edu>
2023-05-09 16:52:35 -0400

    load all the test data into the thing

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,18 +7,18 @@ from pooch import Untar
 # Download data if necessary
 def pytest_configure():
     fnames = pooch.retrieve(
-        url="https://zenodo.org/record/5832607/files/Data.tar.gz?download=1",
+        url="https://zenodo.org/record/7916559/files/Data.tar.gz?download=1",
         processor=Untar(),
-        known_hash="98b2bfadefa62dd223224c797354f9266b54143c2af3c4b6fe676d8547e7d5ee",
+        known_hash="c27a566ed280f3a579dd2bf68462a427d7d0d1286cdd8db2a4a035495f40f7e4",
     )
     symlink_args = dict(
         src=f"{os.path.commonpath(fnames)}",
-        dst="./oceanspy/tests/Data",
+        dst="./tests/Data",
         target_is_directory=True,
     )
     try:
         print(f"Linking {symlink_args['src']!r} to {symlink_args['dst']!r}")
         os.symlink(**symlink_args)
     except FileExistsError:
-        os.unlink("./oceanspy/tests/Data")
+        os.unlink("./tests/Data")
         os.symlink(**symlink_args)
