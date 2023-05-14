f63d6f263b102aae0f5978b0773d351fb86d3617
wenrui <wjiang33@jhu.edu>
2023-05-14 18:03:43 +0000

    write tests for topology

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -1,5 +1,4 @@
 import numpy as np
-import oceanspy as ospy
 import pytest
 import xarray as xr
 
@@ -7,8 +6,8 @@ import seaduck.kernelNweight as kw
 from seaduck.topology import topology
 
 Datadir = "tests/Data/"
-curv = ospy.open_oceandataset.from_netcdf("{}MITgcm_curv_nc.nc" "".format(Datadir))
-rect = ospy.open_oceandataset.from_netcdf("{}MITgcm_rect_nc.nc" "".format(Datadir))
+curv = xr.open_dataset("{}MITgcm_curv_nc.nc" "".format(Datadir))
+rect = xr.open_dataset("{}MITgcm_rect_nc.nc" "".format(Datadir))
 ecco = xr.open_zarr(Datadir + "small_ecco")
 
 tp = topology(ecco)
@@ -74,6 +73,13 @@ def test_mutual_face():
 def test_llc_ind_tend(ind, tend, result):
     res = tp.ind_tend(ind, tend)
     assert res == result
+    
+@pytest.mark.parametrize(
+    'ds',[curv,rect]
+)
+def test_other_ind_tend(ds):
+    temp_tp = topology(ds)
+    temp_tp.ind_tend((0,0),3)
 
 mundane = np.array([[[1.0, 1.0]], [[0.0, 0.0]], [[0.0, -0.0]], [[1.0, 1.0]]])
 
@@ -93,6 +99,56 @@ def test_4_matrix(fface, cis):
         assert np.allclose(ans, mundane)
     else:
         assert not np.allclose(ans, mundane)
+        
+@pytest.mark.parametrize(
+    'stmt,error',[
+        ("tp.ind_tend((1,45,45),0,cuvg = 'G')",NotImplementedError),
+        ("tp.ind_tend((1,45,45),0,cuvg = 'other')",ValueError),
+        ("tp.ind_moves((1,45,45),['left','left'])",ValueError)
+    ]
+)
+def test_other_errors(stmt,error):
+    with pytest.raises(error):
+        eval(stmt)
+        
+def test_ind_moves_with1illegal():
+    tp.ind_moves((1,-1,89),[0,0])
+    
+@pytest.mark.parametrize(
+    'ind,tend,ans',[
+        ((1,45,45),0,(1 ,46,45)),
+        ((1,45,45),2,(1 ,45,44)),
+        ((1,0 ,0 ),2,(12,89,0 )),
+        ((1,0 ,0 ),1,(0 ,89,0 )),
+        ((4,45,89),3,(8 ,0 ,45)),
+    ]
+)
+def test_ind_tend_v(ind,tend,ans):
+    res = tp.ind_tend(ind,tend,cuvg = 'V')
+    assert res == ans
+    
+@pytest.mark.parametrize(
+    'ans,tend,ind',[
+        ((1,45,45),1,(1 ,46,45)),
+        ((1,45,44),2,(1 ,45,45)),
+        ((1,0 ,0 ),0,(12,89,0 )),
+        ((4,45,89),1,(8 ,0 ,45)),
+    ]
+)
+def test_ind_tend_u(ind,tend,ans):
+    res = tp.ind_tend(ind,tend,cuvg = 'U')
+    assert res == ans
+    
+def test_wall_between():
+    # it is a bit hard to think about an example
+    # that uses this case from higher level. 
+    uv,R = tp._find_wall_between((11,0,45),(8,89,45))
+    assert uv == 'V'
+    assert R == (11,0,45)
+    # It does not make sense to parametrize now. 
+    uv,R = tp._find_wall_between((8,45,0),(7,45,89))
+    assert uv == 'U'
+    assert R == (8,45,0 )
 
 
 # def test_fatten_ind_h_ecco():
