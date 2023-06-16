ed389bffc7096d23ff25e2a05de50b21fcf7d859
Wenrui Jiang <wjiang33@jhu.edu>
2023-06-15 21:24:28 -0400

    reduce alias a little, see if I can push

--- tests/test_smart_read.py
+++ tests/test_smart_read.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from seaduck.smart_read import smart_read as srd
+from seaduck.smart_read import smart_read
 
 
 @pytest.fixture
@@ -19,16 +19,16 @@ def ind():
 @pytest.mark.parametrize("ds", ["ecco"], indirect=True)
 def test_just_read(ind, ds, chunk):
     ds["SALT"] = ds["SALT"].chunk(chunk)
-    srd(ds["SALT"], ind)
+    smart_read(ds["SALT"], ind)
 
 
 @pytest.mark.parametrize("ds", ["ecco"], indirect=True)
 def test_read_xarray(ind, ds):
     ds["SALT"] = ds["SALT"].chunk({"time": 1})
-    srd(ds["SALT"], ind, dask_more_efficient=1)
+    smart_read(ds["SALT"], ind, dask_more_efficient=1)
 
 
 @pytest.mark.parametrize("ds", ["ecco"], indirect=True)
 def test_mismatch_read(ind, ds):
     with pytest.raises(ValueError):
-        srd(ds["XC"], ind)
+        smart_read(ds["XC"], ind)
