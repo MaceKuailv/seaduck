6d55a8b6845ca7d6e7efd1a6bc8492c0fadbb845
Wenrui Jiang <wjiang33@jhu.edu>
2023-06-19 20:32:38 -0400

    the part that break cell wall cross narrowed

--- tests/test_lagrangian.py
+++ tests/test_lagrangian.py
@@ -24,12 +24,9 @@ z = None
 zz = np.ones_like(x) * (-10.0)
 
 start_time = "1992-02-01"
-t = (
-    np.array([np.datetime64(start_time) for i in x]) - np.datetime64("1970-01-01")
-) / np.timedelta64(1, "s")
-tf = (np.datetime64("1992-03-03") - np.datetime64("1970-01-01")) / np.timedelta64(
-    1, "s"
-)
+t = utils.convert_time(start_time) * np.ones_like(x)
+end_time = "1992-02-03"
+tf = utils.convert_time(end_time)
 
 
 @pytest.fixture
@@ -70,6 +67,18 @@ def test_to_list_of_time(p):
 
 def test_analytical_step(p):
     p.analytical_step(10.0)
+
+
+def test_subset_update(p):
+    np.random.seed(0)
+    which = np.random.randint(1, size=p.N, dtype=bool)
+    sub = p.subset(which)
+    sub.lon += 1
+    sub.lat += 1
+    p.update_from_subset(sub, which)
+    assert isinstance(sub, sd.Particle)
+    assert np.allclose(p.ix[which], sub.ix)
+    assert np.allclose(p.lon[which], sub.lon)
 
 
 @pytest.mark.parametrize("od", ["curv"], indirect=True)
@@ -122,17 +131,16 @@ def test_update_w_array(ecco_p, od):
 
 
 @pytest.mark.parametrize("od", ["ecco"], indirect=True)
-def test_cross_cell_wall(ecco_p, od):
+def test_wall_crossing(ecco_p, od):
     od["SN"] = np.array(od["SN"])
     od["CS"] = np.array(od["CS"])
     ecco_p.ocedata.readiness["h"] = "local_cartesian"
 
-    ecco_p._cross_cell_wall_read()
     ecco_p._cross_cell_wall_rel()
 
 
 @pytest.mark.parametrize("od", ["curv"], indirect=True)
-def test_cross_cell_wall_no_face(od):
+def test_wall_crossing_no_face(od):
     od._add_missing_cs_sn()
     od.readiness["h"] = "local_cartesian"
     curv_p = sd.Particle(
@@ -146,7 +154,6 @@ def test_cross_cell_wall_no_face(od):
         wname="W",
         transport=True,
     )
-    curv_p._cross_cell_wall_read()
     curv_p._cross_cell_wall_rel()
 
 
