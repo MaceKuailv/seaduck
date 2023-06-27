7c30924fa93977b609beeb9ee9cae3a6dcdbf161
Wenrui Jiang <wjiang33@jhu.edu>
2023-06-27 19:01:16 -0400

    assert for test_weight

--- tests/test_weight.py
+++ tests/test_weight.py
@@ -90,17 +90,32 @@ def test_interp_func(kernel, rx, ry):
     np.testing.assert_allclose(np.sum(weight), 1)
 
 
+@pytest.mark.parametrize(
+    "rx,ry",
+    [
+        (np.array([0]), np.array([0])),
+        (np.array([0.5]), np.array([0.08])),
+    ],
+)
 @pytest.mark.parametrize("ktype", ["dx", "dy"])
 @pytest.mark.parametrize("horder", [0, 1])
-def test_create_different_square(ktype, horder):
+def test_create_different_square(ktype, horder, rx, ry):
     k = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    kw.get_func(k, hkernel=ktype, h_order=horder)
+    func = kw.get_func(k, hkernel=ktype, h_order=horder)
+    weight = func(rx, ry)
+    assert weight.shape == (1, len(k))
+    if horder == 0:
+        np.testing.assert_allclose(np.sum(weight), 1)
+    else:
+        np.testing.assert_allclose(np.sum(weight), 0, atol=1e-14)
 
 
 @pytest.mark.parametrize("hkernel", ["dx", "dy"])
 def test_auto_doll(hkernel):
     k = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-    kw.auto_doll(k, hkernel=hkernel)
+    inheritance = kw.auto_doll(k, hkernel=hkernel)
+    assert len(inheritance) > 1
+    assert isinstance(inheritance[0], list)
 
 
 @pytest.mark.parametrize(
@@ -122,7 +137,7 @@ def test_dx(kernel, rx, ry, order):
     func = kw.get_func(kernel, hkernel="dx", h_order=order)
     weight = func(rx, ry)
     assert weight.shape == (1, len(kernel))
-    assert np.allclose(0, np.sum(weight))
+    assert np.allclose(0, np.sum(weight), atol=1e-14)
 
 
 @pytest.mark.parametrize(
@@ -143,7 +158,7 @@ def test_dy(kernel, rx, ry, order):
     func = kw.get_func(kernel, hkernel="dy", h_order=order)
     weight = func(rx, ry)
     assert weight.shape == (1, len(kernel))
-    assert np.allclose(0, np.sum(weight))
+    assert np.allclose(0, np.sum(weight), atol=1e-14)
 
 
 @pytest.mark.parametrize("ktype", ["dx", "dy"])
@@ -182,10 +197,13 @@ def test_order_too_high_error(kernel, order, ktype):
 def test_plot_kernel():
     pytest.importorskip("matplotlib.pyplot")
     kw.show_kernels()
+    # no way to assert here
 
 
 def test_dt_and_bottom_scheme(aknw):
-    aknw.get_weight(np.array([0.618]), np.array([0.0618]))
+    weight = aknw.get_weight(np.array([0.618]), np.array([0.0618]))
+    assert weight.shape[0] == 1
+    assert np.allclose(0, np.sum(weight))
 
 
 @pytest.mark.parametrize("which", ["dx", "dy"])
@@ -200,4 +218,6 @@ def test_maxorder_dxdy_square(which):
     else:
         yorder = 2
     func = kw.kernel_weight_s(kkk, xorder, yorder)
-    func(np.array([0.0]), np.array([0.0]))
+    weight = func(np.array([0.0]), np.array([0.0]))
+    assert weight.shape == (1, len(kkk))
+    assert np.allclose(0, np.sum(weight), atol=1e-14)
