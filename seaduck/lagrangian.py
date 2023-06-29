35b7f3b23ca26e944c174d930a3a251622d7c0a1
Wenrui Jiang <wjiang33@jhu.edu>
2023-06-28 22:02:54 -0400

    some changes before checkout

--- seaduck/lagrangian.py
+++ seaduck/lagrangian.py
@@ -504,7 +504,7 @@ class Particle(Position):
             z_out = False
         return np.logical_or(np.logical_or(x_out, y_out), z_out)
 
-    def trim(self, tol=0.0):
+    def trim(self, tol=1e-14):
         """Move the particles from outside the cell into the cell.
 
         At the same time change the velocity accordingly.
@@ -876,7 +876,7 @@ class Particle(Position):
             logging.info("Nothing left to simulate")
             return
         tf_used = tf[int_todo]
-        trim_tol = 0.0
+        trim_tol = 1e-14
         for i in range(self.max_iteration):
             if i > self.max_iteration*0.95:
                 trim_tol = 1e-3
