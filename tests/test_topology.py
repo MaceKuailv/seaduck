11c62207432f66a63f6967d7f05a6a6db5819cf6
Wenrui Jiang <wjiang33@jhu.edu>
2023-08-17 16:22:19 -0400

    change name so that it does not repeat

--- tests/test_topology.py
+++ tests/test_topology.py
@@ -243,7 +243,7 @@ def test_wall_between_itself(tp):
     [((10, 89, 45), 0, ("U", (2, 44, 0))), ((7, 0, 45), 1, ("U", (5, 44, 89)))],
 )
 @pytest.mark.parametrize("tp", ["ecco"], indirect=True)
-def test_ind_tend_v(tp, ind, tend, exp):
+def test_ind_tend_v_detailed(tp, ind, tend, exp):
     ans = tp._ind_tend_V(ind, tend)
     assert ans == exp
 
@@ -253,6 +253,6 @@ def test_ind_tend_v(tp, ind, tend, exp):
     [((2, 44, 0), 2, ("V", (10, 89, 45))), ((5, 44, 89), 3, ("V", (7, 0, 45)))],
 )
 @pytest.mark.parametrize("tp", ["ecco"], indirect=True)
-def test_ind_tend_u(tp, ind, tend, exp):
+def test_ind_tend_u_detailed(tp, ind, tend, exp):
     ans = tp._ind_tend_U(ind, tend)
     assert ans == exp
