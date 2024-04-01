24fe4f0212830f8deaf72e9861dde52a987cf800
Wenrui Jiang <wjiang33@jhu.edu>
2024-04-01 15:03:07 -0400

    dic advective scheme

--- tests/test_eulerian_budget.py
+++ tests/test_eulerian_budget.py
@@ -5,6 +5,9 @@ from seaduck.eulerian_budget import (
     buffer_x_periodic,
     buffer_y_periodic,
     buffer_z_nearest_withoutface,
+    second_order_flux_limiter_x,
+    second_order_flux_limiter_y,
+    second_order_flux_limiter_z_withoutface,
     superbee_fluxlimiter,
 )
 
@@ -46,3 +49,30 @@ def test_buffer_z_nearest_withoutface(random_4d, lm, rm):
         assert np.allclose(buffer[..., -1, :, :], random_4d[..., -1, :, :])
     if lm != 0:
         assert np.allclose(buffer[..., 0, :, :], random_4d[..., 0, :, :])
+
+
+# The next three tests are not very quantitative,
+# but they has been tested with real datasets,
+# still there needs to be some improvements.
+def test_second_order_flux_limiter_x(random_4d):
+    np.random.seed(401)
+    u_cfl = np.random.random((3, 4, 5, 5)) * 2 - 1
+    ans = second_order_flux_limiter_x(random_4d, u_cfl)
+    assert ans.shape == u_cfl.shape
+    assert ans.dtype == "float64"
+
+
+def test_second_order_flux_limiter_y(random_4d):
+    np.random.seed(401)
+    v_cfl = np.random.random((3, 4, 6, 4)) * 2 - 1
+    ans = second_order_flux_limiter_y(random_4d, v_cfl)
+    assert ans.shape == v_cfl.shape
+    assert ans.dtype == "float64"
+
+
+def test_second_order_flux_limiter_z_withoutface(random_4d):
+    np.random.seed(401)
+    w_cfl = np.random.random((3, 4, 5, 4)) * 2 - 1
+    ans = second_order_flux_limiter_z_withoutface(random_4d, w_cfl)
+    assert ans.shape == w_cfl.shape
+    assert ans.dtype == "float64"
