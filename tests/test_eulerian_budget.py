083c53a791d8b4a536b4dcbe64fd53cfc2c3f9cf
Wenrui Jiang <wjiang33@jhu.edu>
2024-10-04 14:00:44 -0400

    write the tests for xgcm stuff, which turns out to be very bad

--- tests/test_eulerian_budget.py
+++ tests/test_eulerian_budget.py
@@ -1,14 +1,19 @@
 import numpy as np
 import pytest
+import xarray as xr
 
+import seaduck as sd
 from seaduck.eulerian_budget import (
+    bolus_vel_from_psi,
     buffer_x_periodic,
     buffer_y_periodic,
     buffer_z_nearest_withoutface,
+    create_ecco_grid,
     second_order_flux_limiter_x,
     second_order_flux_limiter_y,
     second_order_flux_limiter_z_withoutface,
     superbee_fluxlimiter,
+    total_div,
 )
 
 
@@ -16,6 +21,28 @@ from seaduck.eulerian_budget import (
 def random_4d():
     np.random.seed(401)
     return np.random.random((3, 4, 5, 4))
+
+
+@pytest.fixture
+def grid():
+    od = sd.OceData(sd.utils.get_dataset("ecco"))
+    return create_ecco_grid(od._ds, for_outer=True)
+
+
+@pytest.mark.parametrize("od", ["ecco"], indirect=True)
+def test_total_div(grid, od):
+    total_div(od, grid, "UVELMASS", "VVELMASS", "WVELMASS")
+
+
+@pytest.mark.parametrize("od", ["ecco"], indirect=True)
+def test_bolus_calc(grid, od):
+    od["GM_PsiX"] = xr.DataArray(
+        od["WVELMASS"].data, dims=("time", "Zl", "face", "Y", "Xp1")
+    )
+    od["GM_PsiY"] = xr.DataArray(
+        od["WVELMASS"].data, dims=("time", "Zl", "face", "Yp1", "X")
+    )
+    bolus_vel_from_psi(od, grid, psixname="GM_PsiX", psiyname="GM_PsiY")
 
 
 def test_superbee():
